026c505a296433c3bcf78346eccf603528220530
Amit Panwar <64302444+amitpanwar789@users.noreply.github.com>
2022-12-02 20:25:10 +0530

    title tag updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import SortingVisualizer from './sortingVisualizer/sortingVisualizer';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Sorting Visualizer";
+  }, []);
   return (
     <div className="App">
       <SortingVisualizer></SortingVisualizer>
